0872c482ecec4d16255b35fd6f6461e1d4ed2c89
Fiona <feeonawong@gmail.com>
2017-11-01 16:59:01 -0700

    saving einstein calls to database

--- Components/Main.js
+++ Components/Main.js
@@ -12,18 +12,45 @@ export default class Main extends Component<{}> {
     this.state = {
       picSource: null,
       id: user.uid,
-      probability: null
+      probability: null,
+      photo: null
     };
     this.writeNewPhoto = this.writeNewPhoto.bind(this);
     this.selectPhotoTapped = this.selectPhotoTapped.bind(this);
+    this.updatePhotoCount = this.updatePhotoCount.bind(this);
+    this.handleEinstein = this.handleEinstein.bind(this);
+    this.updatePhotoProb = this.updatePhotoProb.bind(this);
   }
 
-  writeNewPhoto(uid, name, picture) {
+  updatePhotoProb(probabilities, uid, photoCount) {
+    probabilities.forEach(probability => {
+      let userLabelRef = firebase.database().ref(`users/${uid}/${probability.label}`);
+      userLabelRef.transaction(currentProb => {
+        return (currentProb + probability.probability) / photoCount;
+      });
+    });
+  }
+
+  //updates users analysis in the database
+  updatePhotoCount(probabilities, uid) {
+    let userPhotoCountRef = firebase.database().ref(`users/${uid}/photoCount`);
+    return userPhotoCountRef.transaction(currentPhotoCount => {
+      return currentPhotoCount + 1;
+    }, (error, completed, snapshot) => {
+      if (completed) {
+        this.updatePhotoProb(probabilities, uid, snapshot.val());
+      }
+    });
+  }
+
+  //updates photos in the database
+  writeNewPhoto(uid, name, picture, probabilities) {
     let photoData = {
       author: name,
       uid: uid,
       likesCount: 0,
-      picture: picture
+      picture: picture,
+      probabilities: probabilities
     };
 
     // Get a key for a new Photo.
@@ -32,11 +59,39 @@ export default class Main extends Component<{}> {
     // Write the new photo's data simultaneously in the photos list and the user's photo list.
     let updates = {};
     updates[`/photos/'${newPhotoKey}`] = photoData;
-    updates[`/user-photos/'${uid}/${newPhotoKey}`] = photoData;
+    updates[`/user-photos/${uid}/${newPhotoKey}`] = photoData;
 
+    this.setState({
+      probability: probabilities
+    });
     return firebase.database().ref().update(updates);
   }
 
+  //makes call to einstein api for image analysis
+  handleEinstein(imgString) {
+    //convert data to form data as the acceptable request body
+    let form_data = new FormData();
+    form_data.append('modelId', 'VUAMRM3AOOQGP2SS6EGRSVHN3M');
+    form_data.append('sampleBase64Content', imgString);
+
+    fetch('https://api.einstein.ai/v2/vision/predict', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer OC6A527QGYDS5DNYOY7Z3CACV466RRDUVHWKFW3FJRUC7CTZQGMHDUXSNOG7NBLTU6OKR2TLAMU5KIQHWUQIKO4ZWI6HSPGQGL4NMCY',
+        'Content-Type': 'multipart/form-data',
+        'Cache-Control': 'no-cache'
+      },
+      body: form_data
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.writeNewPhoto(this.state.id, this.props.display, imgString, responseJson.probabilities);
+      this.updatePhotoCount(responseJson.probabilities, this.state.id);
+    })
+    .catch(err => console.log(err));
+  }
+
+  //generates a base64 string for the photo
   selectPhotoTapped() {
     const options = {
       quality: 1.0,
@@ -60,38 +115,11 @@ export default class Main extends Component<{}> {
         console.log('User tapped custom button: ', response.customButton);
       }
       else {
-        let storageRef = firebase.storage().ref();
         let base64Source = {uri: `data:image/jpeg;base64,${response.data}`};
-
         this.setState({
           picSource: base64Source
         });
-
-        //convert data to form data as the acceptable request body
-        let form_data = new FormData();
-        form_data.append('modelId', 'VUAMRM3AOOQGP2SS6EGRSVHN3M');
-        form_data.append('sampleBase64Content', response.data);
-
-
-
-        fetch('https://api.einstein.ai/v2/vision/predict', {
-          method: 'POST',
-          headers: {
-            'Authorization': 'Bearer OC6A527QGYDS5DNYOY7Z3CACV466RRDUVHWKFW3FJRUC7CTZQGMHDUXSNOG7NBLTU6OKR2TLAMU5KIQHWUQIKO4ZWI6HSPGQGL4NMCY',
-            'Content-Type': 'multipart/form-data',
-            'Cache-Control': 'no-cache'
-          },
-          body: form_data
-        })
-        .then((response) => response.json())
-        .then((responseJson) => {
-          this.setState({
-            probability: responseJson.probabilities
-          })
-        })
-        .catch(err => console.log(err));
-
-        this.writeNewPhoto(this.state.id, this.props.display, response.data);
+        this.handleEinstein(response.data);
       }
     });
   }
@@ -138,10 +166,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#000000'
   },
   avatarContainer: {
-    borderColor: '#9B9B9B',
+    borderColor: '#A8C73F',
     borderWidth: 1 / PixelRatio.get(),
     justifyContent: 'center',
     alignItems: 'center'
